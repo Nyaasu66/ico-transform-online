31eb735d261630eb261bc17d3ba90b08e6812eac
Nyaasu66 <HiNyaasu@outlook.com>
2024-06-12 18:31:43 +0800

    ✨ feat: 接入谷歌统计

--- index.php
+++ index.php
@@ -37,6 +37,18 @@ include("./favicon.php");
 			reader.readAsDataURL(file);
 		}
 	</script>
+	<!-- Google tag (gtag.js) -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-MQ5XKRJNTF"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+
+		function gtag() {
+			dataLayer.push(arguments);
+		}
+		gtag('js', new Date());
+
+		gtag('config', 'G-MQ5XKRJNTF');
+	</script>
 </head>
 
 <body>
